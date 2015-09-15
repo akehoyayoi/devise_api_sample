7a9dbfd185fdf3f5a603676a69599773e0383258
Yohei Okaya <okaya.dcinc@gmail.com>
2015-09-15 17:01:33 +0900

    fix typo

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,7 +2,7 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    can :managem, Note, user: user if user
+    can :manage, Note, user: user if user
 
     # Define abilities for the passed in user here. For example:
     #
